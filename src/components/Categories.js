6d2f750526a9dcba461b7ba095079c2ac116062b
rajchatterz <iamraj.space@gmail.com>
2023-12-05 22:57:30 +0530

    finished with the card banner styling

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -7,7 +7,7 @@ const Categories = () => {
           <Text style={{fontSize:25,fontWeight:'900',paddingBottom:9,color:'#874fc4'}}>Categories</Text>
           <ScrollView showsHorizontalScrollIndicator={ false} horizontal>
               {categories.map((item,index)=>(
-                  <View style={{marginRight:39,elevation:100}} key={item.id}>
+                  <View style={{marginRight:20,elevation:100}} key={item.id}>
                     <Text style={{backgroundColor:index===0?'#874fc4':'white',borderRadius:8,paddingHorizontal:16,paddingVertical:9,color:index===0?'white':'black',fontSize:18,fontWeight:'500'}}>{item.category}</Text>
                 </View>
               ))}
--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,13 +1,23 @@
 import { StyleSheet, Text, View,FlatList, Image } from 'react-native'
 import React from 'react'
 import { recipeList } from './Constant'
+import { FontAwesome } from '@expo/vector-icons'
 const RecipeCard = () => {
   return (
     <View>
-          <FlatList data={recipeList} renderItem={({ item }) => <View style={{ backgroundColor: 'white', elevation: 100, shadowColor: 'white', shadowOpacity: 0.2, shadowRadius: 2, marginVertical: 16, borderRadius: 3, alignItems: 'center' }}>
-              <Image style={{width:150,height:150,resizeMode:'center'}} source={item.image}/>
-              <Text>{ item.name}</Text>
-      </View>}/>
+          <FlatList data={recipeList} renderItem={({ item }) => <View style={{ backgroundColor: 'white', elevation: 100, shadowColor: 'white',shadowOffset:{width:0.2,height:1}, shadowOpacity: 0.2, shadowRadius: 2, marginVertical: 16, borderRadius: 3, alignItems: 'center', paddingHorizontal: 9, paddingVertical: 30,borderRadius:12 }}>
+              <Image style={{ width: 150, height: 150, resizeMode: 'center' }} source={item.image} />
+              <Text>{item.name}</Text>
+              <View style={{flexDirection:'row',marginTop:8}}>
+                  <Text>{item.time}</Text>
+                      <Text> | </Text>
+                  <View style={{flexDirection:'row', justifyContent:'center',alignItems:'center'}}>
+                      <Text style={{marginRight:5}}>{item.rating}</Text>
+                      <FontAwesome name="star" size={16} color={"#874fc4"}/>
+                  </View>
+              </View>
+          </View>} numColumns={2} columnWrapperStyle={{justifyContent:'space-between'}} showsVerticalScrollIndicator={false}
+           />
     </View>
   )
 }
