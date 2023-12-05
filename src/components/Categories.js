6c23806762a4cb23818375ce1fa4fafe7de99d65
rajchatterz <iamraj.space@gmail.com>
2023-12-05 21:00:49 +0530

    import recipe list

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -6,9 +6,9 @@ const Categories = () => {
       <View>
           <Text style={{fontSize:25,fontWeight:'900',paddingBottom:9,color:'#874fc4'}}>Categories</Text>
           <ScrollView showsHorizontalScrollIndicator={ false} horizontal>
-              {categories.map((item)=>(
-                  <View style={{marginRight:39}} key={item.id}>
-                    <Text style={{backgroundColor:'#874fc4',borderRadius:8,paddingHorizontal:16,paddingVertical:9,color:'white',fontSize:18,fontWeight:'900'}}>{item.category}</Text>
+              {categories.map((item,index)=>(
+                  <View style={{marginRight:39,elevation:100}} key={item.id}>
+                    <Text style={{backgroundColor:index===0?'#874fc4':'white',borderRadius:8,paddingHorizontal:16,paddingVertical:9,color:index===0?'white':'black',fontSize:18,fontWeight:'500'}}>{item.category}</Text>
                 </View>
               ))}
         </ScrollView>
